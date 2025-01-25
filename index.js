839fad41a0d7c27a377313c442602ca32d776ff9
Rojenn Clyde Patigdas <patigdasr@gmail.com>
2025-01-25 15:51:34 +0800

    added images

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var drumCount = document.querySelectorAll(".drum").length;
 for(var x = 0; x<drumCount; x++){
 
     document.querySelectorAll(".drum")[x].addEventListener("click", function (){
-        alert(x);
+        var audio = new Audio('./sounds/tom-1.mp3');
+        audio.play();
     });
 
 }
