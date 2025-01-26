26106ff4688f0d8f2be83a9d419920c95beee610
Rojenn Clyde Patigdas <patigdasr@gmail.com>
2025-01-27 07:12:20 +0800

    code refactoring

--- index.js
+++ index.js
@@ -5,60 +5,25 @@ for(var x = 0; x<drumCount; x++){
     document.querySelectorAll(".drum")[x].addEventListener("click", function (){
 
         var buttoninnerHTML = this.innerHTML;
-        switch(buttoninnerHTML){
-            case "w":
-                var crash = new Audio('./sounds/crash.mp3');
-                crash.play();
-            break;
-
-            case "a":
-                var snare = new Audio('./sounds/snare.mp3');
-                snare.play();
-            break;
-
-            case "s":
-                var tom1 = new Audio('./sounds/tom-1.mp3');
-                tom1.play();
-            break;
-
-            case "d":
-                var kick = new Audio('./sounds/kick-bass.mp3');
-                kick.play();
-            break;
-
-            case "j":
-                var tom2 = new Audio('./sounds/tom-2.mp3');
-                tom2.play();
-            break;
-
-            case "k":
-                var tom3 = new Audio('./sounds/tom-3.mp3');
-                tom3.play();
-            break;
-
-            case "l":
-                var tom4 = new Audio('./sounds/tom-4.mp3');
-                tom4.play();
-            break;
-
-            default: console.log(buttoninnerHTML);
-            
-
-        }
-
-
+        
+        makeAudio(buttoninnerHTML);
     });
-
-
-
-
-
 }
+
 
 document.addEventListener("keydown", function (event){
 
+    var keypress = event.key;
 
-    switch(event.key){
+    makeAudio(keypress);
+
+
+});
+
+
+
+function makeAudio(key){
+    switch(key){
         case "w":
             var crash = new Audio('./sounds/crash.mp3');
             crash.play();
@@ -94,12 +59,8 @@ document.addEventListener("keydown", function (event){
             tom4.play();
         break;
 
-        default: console.log(event.key);
+        default: console.log(key);
         
 
     }
-
-
-});
-
-
+}
